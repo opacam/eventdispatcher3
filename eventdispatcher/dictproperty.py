18ae0d6d3d1bed1aee36a790f2ab7077b33f5ea1
opacam <canellestudi@gmail.com>
2023-02-27 12:59:27 +0100

    :label: Add type annotations to dictproperty.py file
    In order to improve code clarity and maintainability
    
    We also:
      - :alien: Use Python 3 super() for the DictProperty class
      - :recycle: simplify import of `MutableMapping` because we don't need to support old versions of Python
      - :rotating_light: Reduce long lines to be below 80 characters
    
    **Warning:** This commit drops support for Python versions below 3.6

--- eventdispatcher/dictproperty.py
+++ eventdispatcher/dictproperty.py
@@ -1,11 +1,10 @@
 __author__ = 'calvin'
-try:
-    # python >= 3.3
-    from collections.abc import MutableMapping
-except ImportError:
-    from collections import MutableMapping
+
+from collections.abc import MutableMapping
 from future.utils import iteritems, iterkeys, itervalues
 from functools import partial
+from typing import Any, Callable, Dict, Optional, Tuple
+
 from . import Property
 
 
@@ -15,83 +14,83 @@ class __DoesNotExist__:
 
 
 class ObservableDict(MutableMapping):
+    def __init__(self, dictionary: Dict, dispatch_method: Callable) -> None:
+        self.dictionary: Dict = dictionary.copy()
+        self.dispatch: Callable = dispatch_method
 
-    def __init__(self, dictionary, dispatch_method):
-        self.dictionary = dictionary.copy()
-        self.dispatch = dispatch_method
-
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.dictionary.__repr__()
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: Any, owner: Any) -> Dict:
         return self.dictionary
 
-    def __contains__(self, item):
+    def __contains__(self, item: Any) -> bool:
         return item in self.dictionary
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: Any) -> Any:
         return self.dictionary[item]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: Any, value: Any) -> None:
         prev = self.dictionary.get(key, __DoesNotExist__)
         self.dictionary[key] = value
         try:
-            # Ensure that the comparison evaluates as a scalar boolean (unlike numpy arrrays)
+            # Ensure that the comparison evaluates as a
+            # scalar boolean (unlike numpy arrays)
             dispatch = bool(prev != value)
         except Exception:
             dispatch = True
         if dispatch:
             self.dispatch(self.dictionary)
 
-    def clear(self):
+    def clear(self) -> None:
         if len(self.dictionary):
             self.dictionary.clear()
             self.dispatch(self.dictionary)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.dictionary)
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any) -> bool:
         return self.dictionary == other
 
-    def __cmp__(self, other):
+    def __cmp__(self, other: Any) -> bool:
         return self.dictionary == other
 
-    def __ne__(self, other):
+    def __ne__(self, other: Any) -> bool:
         return self.dictionary != other
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: Any) -> None:
         del self.dictionary[key]
         self.dispatch(self.dictionary)
 
-    def __iter__(self):
+    def __iter__(self) -> Any:
         return iter(self.dictionary)
 
-    def __nonzero__(self):
+    def __nonzero__(self) -> bool:
         return bool(self.dictionary)
 
-    def __getstate__(self):
+    def __getstate__(self) -> Dict:
         return self.dictionary
 
-    def __reduce__(self):
+    def __reduce__(self) -> Tuple:
         return dict, tuple(), None, None, iter(iteritems(self.dictionary))
 
-    def copy(self):
+    def copy(self) -> Dict:
         return self.dictionary.copy()
 
-    def get(self, key, default=None):
+    def get(self, key: Any, default: Optional[Any] = None) -> Any:
         return self.dictionary.get(key, default)
 
-    def itervalues(self):
+    def itervalues(self) -> Any:
         return itervalues(self.dictionary)
 
-    def iterkeys(self):
+    def iterkeys(self) -> Any:
         return iterkeys(self.dictionary)
 
-    def iteritems(self):
+    def iteritems(self) -> Any:
         return iteritems(self.dictionary)
 
-    def update(self, _dict=None, **kwargs):
+    def update(self, _dict: Optional[Dict] = None, **kwargs: Any) -> None:
         if _dict:
             try:
                 not_equal = bool(self.dictionary != _dict)
@@ -109,42 +108,48 @@ class ObservableDict(MutableMapping):
                 self.dictionary.update(kwargs)
                 self.dispatch(self.dictionary)
 
-    def keys(self):
+    def keys(self) -> Any:
         return self.dictionary.keys()
 
-    def values(self):
+    def values(self) -> Any:
         return self.dictionary.values()
 
-    def items(self):
+    def items(self) -> Any:
         return self.dictionary.items()
 
-    def pop(self, key):
+    def pop(self, key: Any) -> Any:
         item = self.dictionary.pop(key)
         self.dispatch(self.dictionary)
         return item
 
 
 class DictProperty(Property):
-
-    def __init__(self, default_value, **kwargs):
-        super(DictProperty, self).__init__(default_value, **kwargs)
+    def __init__(self, default_value: Dict, **kwargs: Any) -> None:
+        super().__init__(default_value, **kwargs)
         if not isinstance(default_value, dict):
             raise ValueError('DictProperty takes dict only.')
 
-    def register(self, instance, property_name, value):
-        self.value = ObservableDict(value, dispatch_method=partial(instance.dispatch, property_name, instance))
-        super(DictProperty, self).register(instance, property_name, self.value)
+    def register(self, instance: Any, property_name: str, value: Dict) -> None:
+        self.value = ObservableDict(
+            value,
+            dispatch_method=partial(
+                instance.dispatch, property_name, instance
+            ),
+        )
+        super().register(instance, property_name, self.value)
 
-    def __set__(self, obj, value):
+    def __set__(self, obj: Any, value: Dict) -> None:
         p = self.instances[obj]
         try:
-            # Ensure that the comparison evaluates as a scalar boolean (unlike numpy arrrays)
+            # Ensure that the comparison evaluates as a
+            # scalar boolean (unlike numpy arrrays)
             do_dispatch = bool(p['value'] != value)
         except Exception:
             do_dispatch = True
         if do_dispatch:
             p['value'].dictionary.clear()
-            p['value'].dictionary.update(value)          # Assign to the ObservableDict's value
+            # Assign to the ObservableDict's value
+            p['value'].dictionary.update(value)
             for callback in p['callbacks']:
                 if callback(obj, value):
                     break
