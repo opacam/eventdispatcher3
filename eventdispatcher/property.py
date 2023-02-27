23fc24acadbed1f7b2e1d944191ed11cc8781134
opacam <canellestudi@gmail.com>
2023-02-27 18:43:54 +0100

    :label: Add type annotations to Property class
    In order to improve code clarity and maintainability
    
    We also:
      - :recycle: simplify import of `MutableMapping` because we don't need to support old versions of Python
      - :rotating_light: Reduce long lines to be below 80 charactersrs
      - :art: format the code to be more readable (using black)
    
    **Warning:** This commit drops support for Python versions below 3.6

--- eventdispatcher/property.py
+++ eventdispatcher/property.py
@@ -1,35 +1,42 @@
-__author__ = 'calvin'
+__author__ = "calvin"
 
 
 class Property(object):
-
-    def __init__(self, default_value, **additionals):
+    def __init__(self, default_value: object, **additionals: dict) -> None:
         self.instances = {}
         self.default_value = default_value
         self._additionals = additionals
 
-    def __get__(self, obj, objtype=None):
-        return obj.event_dispatcher_properties[self.name]['value']
+    def __get__(self, obj: object, objtype: type = None) -> object:
+        return obj.event_dispatcher_properties[self.name]["value"]
 
-    def __set__(self, obj, value):
-        if value != obj.event_dispatcher_properties[self.name]['value']:
+    def __set__(self, obj: object, value: object) -> None:
+        if value != obj.event_dispatcher_properties[self.name]["value"]:
             prop = obj.event_dispatcher_properties[self.name]
-            prop['value'] = value
-            for callback in prop['callbacks']:
+            prop["value"] = value
+            for callback in prop["callbacks"]:
                 if callback(obj, value):
                     break
 
-    def __delete__(self, obj):
+    def __delete__(self, obj: object) -> None:
         raise AttributeError("Cannot delete properties")
 
-    def register(self, instance, property_name, default_value):
+    def register(
+        self, instance: object, property_name: str, default_value: object
+    ) -> None:
         info = self._additionals.copy()
-        info.update({'property': self, 'value': default_value, 'name': property_name, 'callbacks': []})
-        # Create the instances dictionary at registration so that each class has it's own instance of it.
+        info.update(
+            {
+                "property": self,
+                "value": default_value,
+                "name": property_name,
+                "callbacks": [],
+            }
+        )
+        # Create the instances dictionary at registration
+        # so that each class has it's own instance of it.
         self.instances[instance] = info
         instance.event_dispatcher_properties[property_name] = info
 
-    def get_dispatcher_property(self, property_name):
+    def get_dispatcher_property(self, property_name: str) -> dict:
         return self.instances[self][property_name]
-
-
