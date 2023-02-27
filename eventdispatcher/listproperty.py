a45bf4466c6a8131caec03854d1aa3ce5fdecfd6
opacam <canellestudi@gmail.com>
2023-02-27 18:39:52 +0100

    :label: Add type annotations to listproperty.py file
    In order to improve code clarity and maintainability
    
    We also:
      - :recycle: simplify import of `MutableSequence` because we don't need to support old versions of Python
      - :recycle: Simplify comparision of list/tuple/ObservableList
    
    **Warning:** This commit drops support for Python versions below 3.6

--- eventdispatcher/listproperty.py
+++ eventdispatcher/listproperty.py
@@ -1,23 +1,23 @@
 __author__ = "calvin"
 
-try:
-    # python >= 3.3
-    from collections.abc import MutableSequence
-except ImportError:
-    from collections import MutableSequence
+from collections.abc import MutableSequence
 from functools import partial
 from copy import copy
+from typing import List, Tuple, Union
 import numpy as np
 
 from . import Property
 
 
 class ObservableList(MutableSequence):
-    def __init__(self, target_list, dispatch_method, dtype=None):
+    def __init__(
+        self,
+        target_list: Union[List, Tuple],
+        dispatch_method: callable,
+        dtype: type = None,
+    ) -> None:
         if (
-            not type(target_list) == list
-            and not type(target_list) == tuple
-            and not isinstance(target_list, ObservableList)
+            not isinstance(target_list, (list, tuple, ObservableList))
         ):
             raise ValueError(
                 "Observable list must only be initialized "
@@ -30,16 +30,16 @@ class ObservableList(MutableSequence):
 
         self.dispatch = dispatch_method
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.list.__repr__()
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: object, owner: type) -> list:
         return self.list
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: int) -> any:
         return self.list[item]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: int, value: any) -> None:
         try:
             not_equal = bool(self.list[key] != value)
         except Exception:
@@ -48,57 +48,63 @@ class ObservableList(MutableSequence):
             self.list[key] = value
             self.dispatch(self.list)
 
-    def __reversed__(self):
+    def __reversed__(self) -> iter:
         return reversed(self.list)
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: int) -> None:
         del self.list[key]
         self.dispatch(self.list)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.list)
 
-    def __iter__(self):
+    def __iter__(self) -> iter:
         return iter(self.list)
 
-    def __nonzero__(self):
+    def __nonzero__(self) -> bool:
         return bool(self.list)
 
-    def __getstate__(self):
+    def __getstate__(self) -> list:
         return self.list
 
-    def __reduce__(self):
+    def __reduce__(self) -> tuple:
         return (list, tuple(), None, iter(self.list), None)
 
-    def insert(self, index, value):
+    def insert(self, index: int, value: any) -> None:
         self.list.insert(index, value)
         self.dispatch(self.list)
 
-    def append(self, value):
+    def append(self, value: any) -> None:
         self.list.append(value)
         self.dispatch(self.list)
 
-    def extend(self, values):
+    def extend(self, values: list) -> None:
         self.list.extend(values)
         self.dispatch(self.list)
 
-    def pop(self, index=-1):
+    def pop(self, index: int = -1) -> any:
         value = self.list.pop(index)
         self.dispatch(self.list)
         return value
 
-    def copy(self):
+    def copy(self) -> list:
         return copy(self.list)
 
-    def __eq__(self, other):
+    def __eq__(self, other: list) -> bool:
         return self.list == other
 
-    def __ne__(self, other):
+    def __ne__(self, other: list) -> bool:
         return self.list != other
 
 
 class ListProperty(Property):
-    def register(self, instance, property_name, value, dtype=None):
+    def register(
+        self,
+        instance: object,
+        property_name: str,
+        value: list,
+        dtype: type = None,
+    ) -> None:
         self.value = ObservableList(
             value,
             dispatch_method=partial(
@@ -108,7 +114,7 @@ class ListProperty(Property):
         )
         super(ListProperty, self).register(instance, property_name, self.value)
 
-    def __set__(self, obj, value):
+    def __set__(self, obj: object, value: list) -> None:
         p = self.instances[obj]
         # Check if we need to dispatch
         do_dispatch = len(p["value"].list) != len(
@@ -122,7 +128,7 @@ class ListProperty(Property):
                     break
 
     @staticmethod
-    def compare_sequences(iter1, iter2):
+    def compare_sequences(iter1: list, iter2: list) -> bool:
         """
         Compares two iterators to determine if they are equal.
         Used to compare lists and tuples.
