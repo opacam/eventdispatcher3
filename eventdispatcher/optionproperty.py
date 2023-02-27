cee08f3fed54673daec4c1cb5e032cc5e5233ef7
opacam <canellestudi@gmail.com>
2023-02-27 18:42:42 +0100

    :label: Add type annotations to OptionProperty class
    In order to improve code clarity and maintainability
    
    We also:
      - :rotating_light: Reduce long lines to be below 80 charactersrs
      - :art: replaces single quotes with double quotes (because we use the black formatter to format the code)
    
    **Warning:** This commit drops support for Python versions below 3.6

--- eventdispatcher/optionproperty.py
+++ eventdispatcher/optionproperty.py
@@ -3,15 +3,18 @@ from .exceptions import InvalidOptionError
 
 
 class OptionProperty(Property):
-
-    def __init__(self, default_value, options, handler=None):
-        super(OptionProperty, self).__init__(default_value, options=options, handler=None)
+    def __init__(
+        self, default_value: str, options: list, handler: callable = None
+    ):
+        super().__init__(default_value, options=options, handler=None)
         self.handler = handler
         self.options = set(options) if options else set()
         if default_value not in self.options:
-            raise ValueError('Default value must be one of the defined options.')
+            raise ValueError(
+                "Default value must be one of the defined options."
+            )
 
-    def __set__(self, obj, value):
+    def __set__(self, obj, value: str):
         if value in self.options:
             super(OptionProperty, self).__set__(obj, value)
         elif self.handler:
@@ -20,9 +23,11 @@ class OptionProperty(Property):
             raise InvalidOptionError(value, self.options)
 
     @staticmethod
-    def set_options(inst, name, options):
-        inst.event_dispatcher_properties[name]['options'].options = set(options)
+    def set_options(inst, name: str, options: list):
+        inst.event_dispatcher_properties[name]["options"].options = set(
+            options
+        )
 
     @staticmethod
-    def set_handler(inst, name, handler):
-        inst.event_dispatcher_properties[name]['handler'].handler = handler
+    def set_handler(inst, name: str, handler: callable):
+        inst.event_dispatcher_properties[name]["handler"].handler = handler
