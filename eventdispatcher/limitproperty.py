ed0e3043b23c360051f619cc38cce75548f355be
opacam <canellestudi@gmail.com>
2023-02-27 18:14:01 +0100

    :label: Add type annotations to LimitProperty class
    In order to improve code clarity and maintainability
    
    We also:
      - :rotating_light: Reduce long lines to be below 80 characters
      - :art: replaces single quotes with double quotes (because we use the black formatter to format the code)
    
    **Warning:** This commit drops support for Python versions below 3.6

--- eventdispatcher/limitproperty.py
+++ eventdispatcher/limitproperty.py
@@ -1,28 +1,31 @@
-__author__ = 'calvin'
+__author__ = "calvin"
+
+from typing import Any
 
 from . import Property
 
 
 class LimitProperty(Property):
+    def __init__(self, default_value: Any, min: Any, max: Any):
+        super().__init__(default_value, min=min, max=max)
 
-    def __init__(self, default_value, min, max):
-        super(LimitProperty, self).__init__(default_value, min=min, max=max)
-
-    def __get__(self, obj, objtype=None):
+    def __get__(self, obj: Any, objtype: Any = None) -> Any:
         return obj.event_dispatcher_properties[self.name]['value']
 
-    def __set__(self, obj, value):
+    def __set__(self, obj: Any, value: Any) -> None:
         info = obj.event_dispatcher_properties[self.name]
         if value != info['value']:
             # Clip the value to be within min/max
             if value < info['min']:
-                # Only dispatch if the current value is not already clipped to the minimum
+                # Only dispatch if the current value
+                # is not already clipped to the minimum
                 if info['value'] != info['min']:
                     info['value'] = info['min']
                 else:
                     return
             elif value > info['max']:
-                # Only dispatch if the current value is not already clipped to the maximum
+                # Only dispatch if the current value
+                # is not already clipped to the maximum
                 if info['value'] != info['max']:
                     info['value'] = value = info['max']
                 else:
@@ -34,25 +37,25 @@ class LimitProperty(Property):
                 if callback(obj, value):
                     break
 
-    def __delete__(self, obj):
+    def __delete__(self, obj: Any) -> None:
         raise AttributeError("Cannot delete properties")
 
     @staticmethod
-    def get_min(inst, name):
+    def get_min(inst: Any, name: str) -> Any:
         return inst.event_dispatcher_properties[name]['min']
 
     @staticmethod
-    def set_min(inst, name, new_min):
+    def set_min(inst: Any, name: str, new_min: Any) -> None:
         inst.event_dispatcher_properties[name]['min'] = new_min
         if getattr(inst, name) < new_min:
             setattr(inst, name, new_min)
 
     @staticmethod
-    def get_max(inst, name):
+    def get_max(inst: Any, name: str) -> Any:
         return inst.event_dispatcher_properties[name]['max']
 
     @staticmethod
-    def set_max(inst, name, new_max):
+    def set_max(inst: Any, name: str, new_max: Any) -> None:
         inst.event_dispatcher_properties[name]['max'] = new_max
         if getattr(inst, name) > new_max:
             setattr(inst, name, new_max)
